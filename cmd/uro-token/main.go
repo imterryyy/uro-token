20aa9f82984164ffbaa7ff4b6557c78efc37578e
Bui Duc Huy <duchuy.124dk@gmail.com>
2022-06-14 14:48:12 +0700

    init

---
+++ cmd/uro-token/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+    "os",
+    "github.com/tendermint/starport/starport/pkg/cosmoscmd",
+    "github.com/bui-duc-huy/uro-token/app"
+)
+
+func main() {
+    
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bui-duc-huy/uro-token
+
+go 1.17
